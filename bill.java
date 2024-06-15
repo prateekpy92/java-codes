c0560f58f97a2bef2268116a058cae880c27c635
prateek <140169991+prateekpy92@users.noreply.github.com>
2024-06-15 10:40:17 +0530

    Update bill.java

--- bill.java
+++ bill.java
@@ -4,26 +4,25 @@ import java.util.Scanner;
 
 public class bill {
 
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-		System.out.println("enter the water bill");
-		int consumed= scanner.nextInt();
-		int meter_charged=75;
-		int total_bill;
-		int charge ;
-		if (consumed <= 100) {
-		    charge=consumed*5;
-		    
-		} else if(consumed <= 250) {
-			charge=consumed*10;
-		   
-		}
-		else  {
-			charge=consumed*20;   
-		}
-		total_bill = charge + meter_charged;
-		System.out.println("Total water bill is   :" + total_bill);
-	}
+        System.out.println("Enter the water consumption in units:");
+        int consumed = scanner.nextInt();
+        int meter_charge = 75;
+        int total_bill;
+        int charge;
 
+        if (consumed <= 100) {
+            charge = consumed * 5;
+        } else if (consumed <= 250) {
+            charge = 100 * 5 + (consumed - 100) * 10;
+        } else {
+            charge = 100 * 5 + 150 * 10 + (consumed - 250) * 20;
+        }
+
+        total_bill = charge + meter_charge;
+        System.out.println("Total water bill is: " + total_bill);
+    }
 }
+
